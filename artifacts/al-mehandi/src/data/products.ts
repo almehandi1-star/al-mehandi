fe83f8125aa7bd2f8e058c0935809d215075347b
almehandi1-star <almehandi1@gmail.com>
2026-03-19 02:18:09 +0530

    Update products.ts

--- artifacts/al-mehandi/src/data/products.ts
+++ artifacts/al-mehandi/src/data/products.ts
@@ -9,37 +9,37 @@ export const combos: ComboProduct[] = [
   {
     comboId: 1,
     name: "Combo 1",
-    price: 70,
+    price: 100,
     items: ["1 Henna Cone", "1 Nail Cone"],
   },
   {
     comboId: 2,
     name: "Combo 2",
-    price: 100,
+    price: 150,
     items: ["1 Henna Cone", "2 Nail Cones"],
   },
   {
     comboId: 3,
     name: "Combo 3",
-    price: 110,
+    price: 170,
     items: ["2 Henna Cones", "1 Nail Cone"],
   },
   {
     comboId: 4,
     name: "Combo 4",
-    price: 190,
+    price: 240,
     items: ["3 Henna Cones", "2 Nail Cones"],
   },
   {
     comboId: 5,
     name: "Combo 5",
-    price: 250,
+    price: 300,
     items: ["4 Henna Cones", "3 Nail Cones"],
   },
   {
     comboId: 6,
     name: "Combo 6",
-    price: 560,
+    price: 600,
     items: ["10 Henna Cones", "5 Nail Cones"],
   },
 ];
